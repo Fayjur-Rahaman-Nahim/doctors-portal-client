0ef6e92f8f3c0effbb6699fd4a14a30594896b90
Fayjur-Rahaman-Nahim <f.r.nahim10@gmail.com>
2021-11-18 00:39:43 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home/Home/Home';
+import Appointment from './pages/Appointment/Appointment/Appointment';
+import Login from './pages/Login/Login/Login';
+import Register from './pages/Login/Register/Register';
+import AuthProvider from './contexts/AuthProvider/AuthProvider';
+import PrivateRoute from './pages/Login/PrivateRoute/PrivateRoute';
+import Dashboard from './pages/Dashboard/Dashboard/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <PrivateRoute path="/appointment">
+              <Appointment />
+            </PrivateRoute>
+            <PrivateRoute path="/dashboard">
+              <Dashboard />
+            </PrivateRoute>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLtCNRlHNOei03jt3f7wz-mBm_lAwCYXY",
+    authDomain: "doctors-portal-a3956.firebaseapp.com",
+    projectId: "doctors-portal-a3956",
+    storageBucket: "doctors-portal-a3956.appspot.com",
+    messagingSenderId: "1005344265502",
+    appId: "1:1005344265502:web:a1844baf510ed14e44250f"
+};
+export default firebaseConfig;
